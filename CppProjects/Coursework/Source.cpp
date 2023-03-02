8b1b552523afa823ac7fa6d47b04c28777c81219
Clinsen <clinsenmain@gmail.com>
2023-03-02 12:48:49 +0200

    Finished last 2 tasks

--- CppProjects/Coursework/Source.cpp
+++ CppProjects/Coursework/Source.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    /*std::cout << "=================== Real ===================\n\n";
+    std::cout << "=================== Real ===================\n\n";
     Real real1;
     std::cout << "Input value of your object: ";
     std::cin >> real1;
@@ -28,6 +28,29 @@ int main()
     Matrix matrix1(10, 10);
     matrix1.fill_matrix();
     matrix1.print_matrix();
-    std::cout << "\nCalculating norm of the matrix: " << matrix1.calculate_norm() << "\n\n";*/
+    std::cout << "\nCalculating norm of the matrix: " << matrix1.calculate_norm() << "\n\n";
+
+    std::cout << "=================== Vector of Complex Numbers, Dynamic Binding Demonstration ===================\n\n";
+    Real real2(-1.22);
+    Complex complex2(1.4, 5);
+    Complex complex3(2.2, -3.7);
+
+    std::vector<Real*>numbers;
+
+    numbers.emplace_back(&real2);
+    numbers.emplace_back(&complex2);
+    numbers.emplace_back(&complex3);
+
+    Real temp = 0;
+    for (Real* numberPtr : numbers)
+    {
+        std::cout << numberPtr->calculate_norm() << "\n";
+        if (temp < numberPtr->calculate_norm())
+        {
+            temp = numberPtr->calculate_norm();
+        }
+    }
+    std::cout << "\nLargest absolute value: " << temp << "\n\n";
+
     return 0;
 }
