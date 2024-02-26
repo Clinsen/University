97f1f7c745491c418ea529423ac14e54e072a409
Clinsen <clinsenmain@gmail.com>
2024-02-26 15:40:05 +0200

    Initial commit

--- C#Projects/DistributedComputing/3/3/Results.cs
+++
@@ -1,7 +0,0 @@
-﻿class Results {
-    public static void DisplayResults(string computationType, double result, TimeSpan timeSpan) {
-        Console.WriteLine($"\n{computationType} Обчислення:");
-        Console.WriteLine($"{computationType} Скалярний добуток: {result}");
-        Console.WriteLine($"{computationType} Швидкість виконання: {timeSpan}");
-    }
-}
