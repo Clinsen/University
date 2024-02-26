b1110eec7b631d7a780684a2c867336ec160644f
Clinsen <clinsenmain@gmail.com>
2024-02-26 15:39:30 +0200

    Initial commit

---
+++ C#Projects/DistributedComputing/12/12/Evaluation.cs
@@ -0,0 +1,19 @@
+﻿class Evaluation {
+    public static void EvaluateEfficiency(TimeSpan sequentialTime, TimeSpan parallelTime) {
+        double speedup = sequentialTime.TotalMilliseconds / parallelTime.TotalMilliseconds;
+        double efficiency = speedup / Environment.ProcessorCount;
+        Console.WriteLine($"\nЕфективність: {efficiency}");
+    }
+
+    public static void EvaluateAmdahlSpeedup(TimeSpan sequentialTime, TimeSpan parallelTime) {
+        double P = sequentialTime.TotalMilliseconds / parallelTime.TotalMilliseconds;
+        double threadCount = Environment.ProcessorCount;
+        double speedupAmdahl = 1 / ((1 - P) + (P / threadCount));
+        Console.WriteLine($"\nПрискорення (Закон Амдала): {speedupAmdahl}");
+    }
+
+    public static void EvaluateGustafsonSpeedup(TimeSpan sequentialTime, TimeSpan parallelTime) {
+        double speedupGustafson = sequentialTime.TotalMilliseconds / parallelTime.TotalMilliseconds;
+        Console.WriteLine($"Прискорення (Закон Густавсона-Барсіса): {speedupGustafson}");
+    }
+}
---
+++ C#Projects/DistributedComputing/3/3/SequentialComputation.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+class SequentialComputation {
+    public static TimeSpan MeasureTime(int dataLength, ref double result) {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        for (int i = 1; i <= dataLength; i++) {
+            double varA = CalculateA(i);
+            double varB = CalculateB(i);
+            result += varA * varB;
+        }
+        stopwatch.Stop();
+        return stopwatch.Elapsed;
+    }
+
+    public static double CalculateA(int i) {
+        return Math.Pow(-1, i + 1) * (Math.Cos(1.5) * i) / Math.Pow(i, 2);
+    }
+
+    public static double CalculateB(int i) {
+        return 1 + (Math.Log(i) / Math.Pow((2 * (Math.Pow(i, 2)) + 1), 2)) - Math.Pow(Math.Exp(-i), 2);
+    }
+}
