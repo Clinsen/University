74e9fddb4336c86a9a85f30e2cfd238a5f5e571f
Clinsen <clinsenmain@gmail.com>
2024-03-18 17:04:39 +0200

    Added question 8

---
+++ C#Projects/DistributedComputing/МКР/2/Question 8/MatrixOperations.cs
@@ -0,0 +1,50 @@
+﻿public class MatrixOperations {
+    public static double[,] GenerateMatrix(int rows, int columns) {
+        double[,] matrix = new double[rows, columns];
+        Random rand = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i, j] = rand.NextDouble() * 10;
+            }
+        }
+
+        return matrix;
+    }
+
+    // по такій самій схемі це було зроблено в лабораторній, помилуйте грішного
+    public static double[,] MultiplyMatricesBlock(double[,] A, double[,] B, int blockSize) {
+        int m = A.GetLength(0);
+        int n = A.GetLength(1);
+        int l = B.GetLength(1);
+        double[,] C = new double[m, l];
+
+        for (int i = 0; i < m; i += blockSize) {
+            for (int j = 0; j < l; j += blockSize) {
+                for (int k = 0; k < n; k += blockSize) {
+                    for (int ii = i; ii < Math.Min(i + blockSize, m); ii++) {
+                        for (int jj = j; jj < Math.Min(j + blockSize, l); jj++) {
+                            for (int kk = k; kk < Math.Min(k + blockSize, n); kk++) {
+                                C[ii, jj] += A[ii, kk] * B[kk, jj];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return C;
+    }
+
+    public static void PrintMatrix(double[,] matrix) {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                Console.Write(matrix[i, j].ToString("F3") + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C#Projects/DistributedComputing/МКР/2/Question 8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+class Program {
+    static void Main() {
+        Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+
+        // Параметри для експериментів
+        int[] mValues = { 5, 10, 15, 20, 25 }; // Число рядків матриці А
+        int[] nValues = { 5, 10, 15, 20, 25 }; // Число рядків і стовбців матриці B
+        int[] lValues = { 5, 10, 15, 20, 25 }; // Число стовбців матриці B
+        int blockSize = 2;
+
+        foreach (int m in mValues) {
+            foreach (int l in lValues) {
+                Console.WriteLine($"Проводимо експеримент для m={m}, l={l}:");
+                Console.WriteLine("----------------------------------------");
+
+                double[,] A = MatrixOperations.GenerateMatrix(m, nValues[0]);
+                double[,] B = MatrixOperations.GenerateMatrix(nValues[0], l);
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                double[,] C = MatrixOperations.MultiplyMatricesBlock(A, B, blockSize);
+                stopwatch.Stop();
+
+                double a = (Math.Pow(Math.Tan(stopwatch.Elapsed.TotalMilliseconds), 2 * Math.Pow(l + 1, 3))) / m - m + 3;
+                double b = Math.Pow(Math.Sin(6 + l), 0.3) / Math.Pow(stopwatch.Elapsed.TotalMilliseconds, (2 / l)) - Math.Pow(l, 2) - 3;
+                Console.WriteLine($"Час виконання: {stopwatch.Elapsed.TotalMilliseconds} мілісекунд(и)");
+                Console.WriteLine($"Значення формули a: {a}");
+                Console.WriteLine($"Значення формули b: {b}\n");
+
+                Console.WriteLine("Матриця A:");
+                MatrixOperations.PrintMatrix(A);
+
+                Console.WriteLine("\nМатриця B:");
+                MatrixOperations.PrintMatrix(B);
+
+                Console.WriteLine("\nМатриця C:");
+                MatrixOperations.PrintMatrix(C);
+
+                Console.WriteLine("\n\n");
+            }
+        }
+    }
+}
