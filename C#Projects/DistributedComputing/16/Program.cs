c646048936abbf915cab6e31cb23b22115c3c258
Clinsen <clinsenmain@gmail.com>
2024-03-24 00:07:55 +0200

    Initial commit

---
+++ C#Projects/DistributedComputing/16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace SimplexMethod {
+    class Program {
+        static void Main(string[] args) {
+            Console.OutputEncoding = Console.InputEncoding = Encoding.Unicode;
+
+            int[,] table = {
+                {5, 1, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 5},
+                {1, -3, 1, 0, 1, 0, 1},
+                {2, 1, 0, 0, 0, 1, 16}
+            };
+
+            double[,] result = new double[table.GetLength(1) - 1, 1];
+            int[,] table_result;
+            Simplex S = new Simplex(table);
+            table_result = S.Calculate(result);
+
+            Console.WriteLine("Розв'язок симплекс-таблиці:");
+            for (int i = 0; i < table_result.GetLength(0); i++) {
+                for (int j = 0; j < table_result.GetLength(1); j++)
+                    Console.Write(table_result[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Розв'язок:");
+            for (int i = 0; i < result.GetLength(0); i++) {
+                Console.WriteLine("X[" + (i + 1) + "] = " + result[i, 0]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#Projects/DistributedComputing/16/Simplex.cs
@@ -0,0 +1,88 @@
+﻿namespace SimplexMethod {
+    class Simplex {
+        private int[,] tableau;
+        private int numConstraints;
+        private int numOriginalVariables;
+        private int[] basis;
+
+        public Simplex(int[,] table) {
+            tableau = table;
+            numConstraints = table.GetLength(0) - 1;
+            numOriginalVariables = table.GetLength(1) - numConstraints - 1;
+            basis = new int[numConstraints];
+            for (int i = 0; i < numConstraints; i++)
+                basis[i] = numOriginalVariables + i;
+        }
+
+        public int[,] Calculate(double[,] result) {
+            while (true) {
+                int pivotColumn = FindPivotColumn();
+                if (pivotColumn == -1)
+                    break;
+
+                int pivotRow = FindPivotRow(pivotColumn);
+                if (pivotRow == -1)
+                    throw new Exception("Розв'язку немає.");
+
+                ApplyPivot(pivotRow, pivotColumn);
+                basis[pivotRow] = pivotColumn;
+            }
+
+            for (int i = 0; i < numOriginalVariables; i++) {
+                result[i, 0] = tableau[numConstraints, i];
+            }
+
+            return tableau;
+        }
+
+        private int FindPivotColumn() {
+            double minValue = 0;
+            int minIndex = -1;
+            for (int j = 0; j < tableau.GetLength(1) - 1; j++) {
+                if (tableau[numConstraints, j] < minValue) {
+                    minValue = tableau[numConstraints, j];
+                    minIndex = j;
+                }
+            }
+            return minIndex;
+        }
+
+        private int FindPivotRow(int pivotColumn) {
+            double minRatio = double.MaxValue;
+            int minIndex = -1;
+            for (int i = 0; i < numConstraints; i++) {
+                if (tableau[i, pivotColumn] > 0) {
+                    double ratio = (double)tableau[i, tableau.GetLength(1) - 1] / tableau[i, pivotColumn];
+                    if (ratio < minRatio) {
+                        minRatio = ratio;
+                        minIndex = i;
+                    }
+                }
+            }
+            return minIndex;
+        }
+
+        private void ApplyPivot(int pivotRow, int pivotColumn) {
+            for (int i = 0; i < tableau.GetLength(0); i++) {
+                for (int j = 0; j < tableau.GetLength(1); j++) {
+                    if (i != pivotRow && j != pivotColumn) {
+                        tableau[i, j] -= tableau[pivotRow, j] * tableau[i, pivotColumn] / tableau[pivotRow, pivotColumn];
+                    }
+                }
+            }
+
+            for (int i = 0; i < tableau.GetLength(0); i++) {
+                if (i != pivotRow) {
+                    tableau[i, pivotColumn] = 0;
+                }
+            }
+
+            for (int j = 0; j < tableau.GetLength(1); j++) {
+                if (j != pivotColumn) {
+                    tableau[pivotRow, j] /= tableau[pivotRow, pivotColumn];
+                }
+            }
+            tableau[pivotRow, pivotColumn] = 1;
+        }
+    }
+}
